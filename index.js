53c6dd8ff7587d20ed447cac225064ff8ac17c7b
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-02-04 17:24:28 -0500

    Added Test code

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // TODO Include packages required to run this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
 // TODO Create a list of questions to get user input
 const questions = [
     {
@@ -24,5 +27,24 @@ const questions = [
     }
 ];  
 // TODO Create test cases for each shape that returns if the shape created is true according to user input
+// TODO Write a function that renders a shape based on user inputs
+function renderShape(){
+
+}
 // TODO Write a Function that initializes the app
+function logoGenerator() {
+    inquirer.prompt(questions)
+    .then(answers, () => {
+        if (answer.title.lengh < 3) {
+            renderShape();
+        }
+        else {
+            console.log("Invalid Input. Please enter a 3 character length name or acryonym");
+        }
+    })
+    .catch(error => {
+        console.log("Prompt Error", error);
+    })
+};
 // TODO Initialize App
+logoGenerator();
